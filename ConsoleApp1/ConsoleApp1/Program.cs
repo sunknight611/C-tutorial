49b91cef62583e0ca4f711e48f1ef7fc2c4abc66
sunknight611 <819442240@qq.com>
2022-10-02 19:12:12 +0800

    C# tutorial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//String input = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Red; 
+//Console.WriteLine(input);
+DateTime date  = DateTime.Now;
+Console.WriteLine($"当前日期为{date.Year}年{date.Month}月{date.Day}日");
+Console.Beep();
+
+
+
