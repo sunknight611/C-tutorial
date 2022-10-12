b2661933067ee91f01bea9f787c1ad03c669914b
sunknight611 <819442240@qq.com>
2022-10-12 15:31:47 +0800

    updateupdate 10/12

---
+++ ConsoleApp1/ObjectEmbed/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ObjectEmbed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InnerClass innerLClass = new InnerClass();
+            OutClass2 outClass2 = new OutClass2(innerLClass);
+
+        }
+    }
+ //对象包含组合方式
+    class InnerClass
+    {
+
+    }
+
+    class OuterClass
+    {
+        private InnerClass obj = null;
+        public OuterClass()
+        {
+            obj = new InnerClass();
+
+        }
+    }
+
+  //对象协作组合方式
+  class OutClass2
+    {
+        private InnerClass obj = null;
+        public OutClass2(InnerClass outerObj)
+        {
+            obj = outerObj;
+        }
+    }
+}
---
+++ ConsoleApp1/OnetoMoreObjectEmbed/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OnetoMoreObjectEmbed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            One2ManyTest();
+        }
+
+        private static void One2ManyTest()
+        {
+            OnetoMoreClass onetoMoreClass = new OnetoMoreClass();
+
+            for(int i = 0;i < 5; i++)
+            {
+                onetoMoreClass.Add(new InnerClass());
+            }
+
+            onetoMoreClass.Remove(0);
+
+
+            List<InnerClass> InnerList = new List<InnerClass>();
+            for(int i = 0;i < 5; i++)
+            {
+                InnerList.Add(new InnerClass());
+            }
+
+            OnetoMoreClass2<InnerClass> onetoMoreClass2 = new OnetoMoreClass2<InnerClass>(InnerList);
+        }
+    }
+
+    //内部定义一些公有方法实现向其内部集合中增删对象
+    class OnetoMoreClass
+    {
+        private List<InnerClass> objs = new List<InnerClass>();
+
+        public void Add(InnerClass obj)
+        {
+            if(obj != null)
+            {
+                objs.Add(obj);
+            }
+        }
+
+        public void Remove(int index)
+        {
+            if(index >=0 && index < objs.Count)
+            {
+                objs.RemoveAt(index);
+            }
+        }
+    }
+
+    class OnetoMoreClass2<T>
+    {
+        private IEnumerable<T> objs = null;
+
+        public OnetoMoreClass2(IEnumerable<T> objCollections)
+        {
+           objs = objCollections;
+        }
+    }
+    class InnerClass
+    {
+        public InnerClass()
+        {
+
+        }
+    }
+}
