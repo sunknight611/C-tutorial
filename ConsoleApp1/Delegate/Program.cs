ed657590944a73868103983721fdffbfa7d91055
sunknight611 <819442240@qq.com>
2022-10-13 23:10:55 +0800

    10/13

---
+++ ConsoleApp1/Delegate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Security.Cryptography;
+using System;
+namespace Delegate
+{  public class MathOpt
+   {
+            public int Add(int x,int y)
+            {
+                return x + y;
+            }
+
+            public static int Max(int x,int y)
+            {
+                return x > y ? x : y; 
+            }
+
+    }
+
+    public class TaskInfo
+    {
+        public int count = 0;
+    }
+    
+    public delegate int MathOptDelegate(int value1, int value2);
+    internal class Program
+    {
+      
+        static int UseDelegate(MathOptDelegate option,int x,int y)
+        {
+            return option(x,y);
+        }
+        
+        static void Main(string[] args)
+        {
+            //MathOptDelegate obj;
+            //MathOpt math = new MathOpt();
+            //obj = math.Add;
+            //obj = MathOpt.Max;
+            //Console.WriteLine(obj(1,2));
+            //Console.WriteLine(UseDelegate(MathOpt.Max, 2, 3));
+            TaskInfo ti = new TaskInfo();
+            Timer timer = new Timer(ShowTime, ti, 0, 1000);
+            Console.ReadKey();
+            timer.Dispose();
+        }
+
+        static void ShowTime(Object ti)
+        {
+            TaskInfo obj = ti as TaskInfo;
+            obj.count++;
+            System.Console.WriteLine("({0}){1}",obj.count,DateTime.Now);
+        }
+    }
+}
---
+++ ConsoleApp1/ObjectCloneViaSerialization/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ObjectCloneViaSerialization
+{
+    [Serializable]
+    class MyClass
+    {
+        public int Index = 1;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj= new MyClass();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                IFormatter formator = new BinaryFormatter();
+                formator.Serialize(ms, obj);
+                for(int i = 0; i < 100; i++)
+                {   //回到流的开头
+                    ms.Seek(0,SeekOrigin.Begin);
+                    //反序列化对象 
+                    obj = (formator.Deserialize(ms) as MyClass);
+                    obj.Index += i;
+                    Console.WriteLine(obj.Index);
+                }
+            }
+        }
+    }
+}
